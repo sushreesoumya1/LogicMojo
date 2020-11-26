a18e09a8d5153e617fd9396d27aa6b652ed891a5
Sushree Soumya Mishra <sushreesoumya.mishra@gmail.com>
2020-11-26 21:50:55 +0530

    BinarySearch Implementation

--- src/ds/array/BinarySearchImpl.java
+++ src/ds/array/BinarySearchImpl.java
@@ -3,8 +3,25 @@ package ds.array;
 public class BinarySearchImpl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr= {1,2,3,4,5,6,7};
+		int index= BinarySearchImpl.BinarySearch(arr,6,0,6);
+		System.out.println(index);
+		
 	}
-
+	
+	public static int BinarySearch(int[] arr, int num, int start, int end) {
+		int mid= start+((end-start)/2); 
+		
+		if(num==arr[mid]) {
+			return mid;
+		}
+		else if(num<arr[mid]) {
+			end= mid-1;
+			return BinarySearch(arr, num, start, end);
+		}
+		else {
+			start=mid+1;
+			return BinarySearch(arr, num, start, end);
+		}
+	}
 }
